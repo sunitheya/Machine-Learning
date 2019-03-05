2379ad83b2cacc8c5b1644c3b2bc5c2aafbd6a98
sunitheya <36173036+sunitheya@users.noreply.github.com>
2019-03-05 13:48:21 +0200

    Update data_preprocessing_template.py

--- Data Preprocessing/data_preprocessing_template.py
+++ Data Preprocessing/data_preprocessing_template.py
@@ -5,13 +5,13 @@ import pandas as pd
 #import dataset
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values      #dependant variables
-y = dataset.iloc[:,3].values         #independent variable(if customer bought the product)
+y = dataset.iloc[:,3].values         #independent variable
 
 #taking care of missing data
-from sklearn.preprocessing import Imputer    #imputer allows to take care of missin data
+from sklearn.preprocessing import Imputer    #imputer allows to take care of missing data
 imputer = Imputer(missing_values='NaN',strategy='mean',axis=0)
-imputer.fit(X[:,1:3])                       #selecting only first two columns in X
-X[:,1:3] = imputer.transform(X[:,1:3])                                #to fill missing data
+imputer.fit(X[:,1:3])                       
+X[:,1:3] = imputer.transform(X[:,1:3])                                
 
 #encoding categorical data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
